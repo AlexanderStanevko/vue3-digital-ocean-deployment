5874296e4dee6c12e0e936c5bba7486876f2dac5
Alexander Stanevko <dardawill@yandex.ru>
2024-01-19 09:37:27 +0300

    change setActiveProd env scrpt

--- setActiveProdEnv.sh
+++ setActiveProdEnv.sh
@@ -5,22 +5,30 @@ set -x
 echo "$SSH_PRIVATE_KEY_DIGITAL_OCEAN_LOAD_BALANCER" | tr -d '\r' > load_balancer_key.pem
 chmod 600 load_balancer_key.pem
 
-# Получение текущего активного upstream на load balancer
+# Получение текущего активного сервера
 active_upstream=$(ssh -i load_balancer_key.pem -o StrictHostKeyChecking=no root@$LOAD_BALANCER_SERVER_IP "grep proxy_pass /etc/nginx/nginx.conf")
 
-# Определение активного сервера
+# Определение нового upstream в зависимости от активного сервера
 if [[ $active_upstream == *"prod_blue"* ]]; then
-  active_server=$PROD_1_IP
-  target_server=$PROD_2_IP
-  target_ssh_key=$SSH_PRIVATE_KEY_DIGITAL_OCEAN_PROD_2
+  new_upstream="prod_green"
+  new_active_server_ip=$PROD_2_IP
+  new_active_server_ssh_key=$SSH_PRIVATE_KEY_DIGITAL_OCEAN_PROD_2
 else
-  active_server=$PROD_2_IP
-  target_server=$PROD_1_IP
-  target_ssh_key=$SSH_PRIVATE_KEY_DIGITAL_OCEAN_PROD_1
+  new_upstream="prod_blue"
+  new_active_server_ip=$PROD_1_IP
+  new_active_server_ssh_key=$SSH_PRIVATE_KEY_DIGITAL_OCEAN_PROD_1
 fi
 
-# Установка переменных окружения
-echo "SERVER_IP=$target_server" >> $GITHUB_ENV
-echo "SSH_PRIVATE_KEY=$(echo "$target_ssh_key" | tr -d '\r')" >> $GITHUB_ENV
+# Обновление конфигурации Nginx на load balancer
+ssh -i load_balancer_key.pem -o StrictHostKeyChecking=no root@$LOAD_BALANCER_SERVER_IP "
+  sed -i 's/proxy_pass http:\/\/prod_.*;/proxy_pass http:\/\/$new_upstream;/' /etc/nginx/nginx.conf
+  nginx -s reload
+"
+
+# Обновление переменной окружения для активного сервера
+echo "SERVER_IP=$new_active_server_ip" >> $GITHUB_ENV
+echo "SSH_PRIVATE_KEY<<EOF" >> $GITHUB_ENV
+echo "$new_active_server_ssh_key" | tr -d '\r' >> $GITHUB_ENV
+echo "EOF" >> $GITHUB_ENV
 
 set +x
